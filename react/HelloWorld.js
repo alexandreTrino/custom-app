c1cac2c93aa7626234b25c9457f6c229b754d4b9
alexandreTrino <trino.alexandre@gmail.com>
2021-05-13 12:29:19 -0500

    first commit

---
+++ react/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HelloWorld = () => {
+
+  return (
+    <React.Fragment>
+      <h1>Hello World</h1>
+    </React.Fragment>
+  )
+}
+
+export default HelloWorld;
